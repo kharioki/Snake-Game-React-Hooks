3c184436d4c73311c55433ecf89660d3294f4d9f
kharioki <antolisah@gmail.com>
2020-04-06 16:28:16 +0300

    gameloop, check collision

--- src/App.js
+++ src/App.js
@@ -18,37 +18,72 @@ function App() {
   const [speed, setSpeed] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const startGame = () => {};
+  useInterval(() => gameLoop(), speed);
 
-  const endGame = () => {};
-
-  const moveSnake = ({ keyCode }) => {
-    keyCode >= 37 && keyCode >= 40 && setDir(DIRECTIONS[keyCode]);
+  const endGame = () => {
+    setSpeed(null);
+    setGameOver(true);
   };
 
-  const createApple = () => {};
+  const moveSnake = ({ keyCode }) =>
+    keyCode >= 37 && keyCode <= 40 && setDir(DIRECTIONS[keyCode]);
 
-  const checkCollision = () => {};
+  const createApple = () =>
+    apple.map((_a, i) => Math.floor(Math.random() * (CANVAS_SIZE[i] / SCALE)));
 
-  const checkAppleCollision = () => {};
+  const checkCollision = (piece, snk = snake) => {
+    if (
+      piece[0] * SCALE >= CANVAS_SIZE[0] ||
+      piece[0] < 0 ||
+      piece[1] * SCALE >= CANVAS_SIZE[1] ||
+      piece[1] < 0
+    )
+      return true;
+
+    for (const segment of snk) {
+      if (piece[0] === segment[0] && piece[1] === segment[1]) return true;
+    }
+    return false;
+  };
+
+  const checkAppleCollision = newSnake => {
+    if (newSnake[0][0] === apple[0] && newSnake[0][1] === apple[1]) {
+      let newApple = createApple();
+      while (checkCollision(newApple, newSnake)) {
+        newApple = createApple();
+      }
+      setApple(newApple);
+      return true;
+    }
+    return false;
+  };
 
   const gameLoop = () => {
     const snakeCopy = JSON.parse(JSON.stringify(snake));
     const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
     snakeCopy.unshift(newSnakeHead);
-    snakeCopy.pop();
+    if (checkCollision(newSnakeHead)) endGame();
+    if (!checkAppleCollision(snakeCopy)) snakeCopy.pop();
     setSnake(snakeCopy);
+  };
+
+  const startGame = () => {
+    setSnake(SNAKE_START);
+    setApple(APPLE_START);
+    setDir([0, -1]);
+    setSpeed(SPEED);
+    setGameOver(false);
   };
 
   useEffect(() => {
     const context = canvasRef.current.getContext("2d");
     context.setTransform(SCALE, 0, 0, SCALE, 0, 0);
-    context.clearRect(0, 0, CANVAS_SIZE[0], CANVAS_SIZE[1]);
-    context.fillStyle ="pink";
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    context.fillStyle = "pink";
     snake.forEach(([x, y]) => context.fillRect(x, y, 1, 1));
-    context.fillStyle="lightBlue";
+    context.fillStyle = "lightblue";
     context.fillRect(apple[0], apple[1], 1, 1);
-  },[snake, apple, gameOver]);
+  }, [snake, apple, gameOver]);
   return (
     <div className="App" role="button" tabIndex="0" onKeyDown={e => moveSnake(e)}>
       <canvas 
