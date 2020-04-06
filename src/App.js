1177de3992199c70744e642dbcdb27fe69d69a75
kharioki <antolisah@gmail.com>
2020-04-06 13:42:44 +0300

    initial canvas states, functions and dimensions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { useInterval } from './useInterval';
+import { 
+  CANVAS_SIZE,
+  SNAKE_START,
+  APPLE_START,
+  SCALE,
+  SPEED,
+  DIRECTIONS
+} from './constants';
 import './App.css';
 
 function App() {
+  const canvasRef = useRef(null);
+  const [snake, setSnake] = useState(SNAKE_START);
+  const [apple, setApple] = useState(APPLE_START);
+  const [dir, setDir] = useState([0, -1]);
+  const [speed, setSpeed] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const startGame = () => {};
+
+  const endGame = () => {};
+
+  const moveSnake = () => {};
+
+  const createApple = () => {};
+
+  const checkCollision = () => {};
+
+  const checkAppleCollision = () => {};
+
+  const gameLoop = () => {};
+
+  useEffect(() => {
+
+  },[snake, apple, gameOver])
   return (
-    <div className="App">
-      Snake Game
+    <div className="App" role="button" tabIndex="0" onKeyDown={e => moveSnake(e)}>
+      <canvas 
+        style={{ border: "1px solid black "}}
+        ref={canvasRef}
+        width={`${CANVAS_SIZE[0]}px`}
+        height={`${CANVAS_SIZE[1]}px`}
+      />
+      {gameOver && <div>GAME OVER!!!</div>}
+      <button onClick={startGame}>Start Game</button>
     </div>
   );
 }
